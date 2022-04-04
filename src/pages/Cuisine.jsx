716f61715e365149c929c6641b5cca26f4faf511
AM Darshan <amdarshan.2001@gmail.com>
2022-04-04 14:26:09 +0530

    Added popular, veggie, routes

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,12 @@
+import React , {useEffect,useState}from 'react'
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+
+function Cuisine() {
+  return (
+    <div>Cuisine</div>
+  )
+}
+
+export default Cuisine
